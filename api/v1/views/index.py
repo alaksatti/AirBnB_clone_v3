a45b21cb05ba531decc7f810d1de01471f567b56
rcolbert30 <colbertrashaad@gmail.com>
2020-01-29 23:35:19 +0000

    index file, w stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 ''' index'''
 from api.v1.views import app_views
-from flask import jsonify
-
+from flask import jsonify, request
+from models import storage
 
 @app_views.route('/status')
 def status():
     ''' status of API route '''
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    ''' retrieves the number of each object by type'''
+    if request.method == 'GET':
+        output = {}
+        PLURALS = {
+            "Amenitiy" : "amenities",
+            "City" : "cities",
+            "Place" : "places",
+            "Review" : "reviews",
+            "State" : "states",
+            "User": "users"
+        }
+    for key, value in PLURALS.items():
+        output[value] = storage.count(key)
+    return jsonify(output)

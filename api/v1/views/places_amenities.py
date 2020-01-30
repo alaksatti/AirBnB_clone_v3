f41fca70fe65d63e8181454ce46bb186ec308413
alaksatti <ala.k.satti@gmail.com>
2020-01-30 09:25:07 +0000

    initial commit

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+''' user file '''
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.place import Place
+from models import storage
+from flask import jsonify, abort, request
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_place_amenity(place_id):
+    '''return json of all amenity objects '''
+    place = storage.get('Place', place_id)
+    if place is None:
+        abort(404)
+    amenity = [amenity.to_dict() for amenity in place.amenities]
+    return jsonify(amenity)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place_amenity(place_id, review_id):
+    ''' delete amenity based on its id '''
+    place = storage.get('Place', place_id)
+    if place:
+        amenity = storage.get('Amenity', amenity_id)
+        if amenity:
+            if amenity in place.amenity:
+                place.amenity.remove(amenity)
+                storage.save()
+                return jsonify({}), 200
+    abort(404)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id', methods=['POST'],
+                 strict_slashes=False)
+def create_place_amenity(place_id, amenity_id):
+    '''create a place_amenity'''
+    place = storage.get('Place', place_id)
+    if place:
+        amenity = storage.get('Amenity', amenity_id)
+        if amenity:
+            if amenity in place.amenity:
+                return jsonify(amenity.to_dict()), 200
+            else:
+                place.amenities.append(amenity)
+                storage.save()
+                return jsonify(amenity.to_dict()), 201
+    abort(404)

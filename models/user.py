a3aa8022142506b168d4f30f1d20ad6a3c6dcd0c
alaksatti <ala.k.satti@gmail.com>
2020-01-31 08:06:07 +0000

    fix issue

--- models/user.py
+++ models/user.py
@@ -32,5 +32,5 @@ class User(BaseModel, Base):
     def __setattr__(self, key, value):
         ''' sets value to hash password'''
         if key == 'password':
-            value = v = hashlib.md5(v.encode()).hexdigest()
+            value = hashlib.md5(v.encode()).hexdigest()
         super().__setattr__(key, value)
